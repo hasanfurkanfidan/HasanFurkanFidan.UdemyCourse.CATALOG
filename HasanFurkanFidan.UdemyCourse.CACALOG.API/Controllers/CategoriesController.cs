a0635120eceeaae76de229e9bdf0182e093da23f
hasanfurkanfidan <furkanfidan.job@gmail.com>
2021-05-16 23:08:40 +0300

    Add project files.

---
+++ HasanFurkanFidan.UdemyCourse.CACALOG.API/Controllers/CategoriesController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HasanFurkanFidan.UdemyCourse.CATALOG.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriesController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var list = new List<string>();
+            list.Add("Furkan");
+            return Ok(list);
+        }
+    }
+}
