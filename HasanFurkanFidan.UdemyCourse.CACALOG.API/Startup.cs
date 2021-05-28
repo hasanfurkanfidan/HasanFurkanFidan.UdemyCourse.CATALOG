04c9bd726fb54c6c9a6b2658695fe4f2705f504e
hasanfurkanfidan <furkanfidan.job@gmail.com>
2021-05-28 14:43:15 +0300

    ilerleme sağlandı

--- HasanFurkanFidan.UdemyCourse.CACALOG.API/Startup.cs
+++ HasanFurkanFidan.UdemyCourse.CACALOG.API/Startup.cs
@@ -61,17 +61,14 @@ namespace HasanFurkanFidan.UdemyCourse.CATALOG.API
                 opt.Filters.Add(new AuthorizeFilter());
             }).AddFluentValidation().AddNewtonsoftJson(options =>
     options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore
-); ;
+); 
             services.Configure<DatabaseSettings>(Configuration.GetSection("DatabaseSettings"));
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "HasanFurkanFidan.UdemyCourse.CAtALOG.API", Version = "v1" });
             });
-            services.AddControllers(opt => {
-                opt.Filters.Add(new AuthorizeFilter());
-            }).AddFluentValidation().AddNewtonsoftJson(options =>
-    options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore
-); ;
+           
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
